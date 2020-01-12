24bf8ffa481df758cfe93da86b1af9c75e674759
Zoe <zoekharvey@gmail.com>
2020-01-12 11:26:50 -0700

    Fix bug in env file, fetch now successfully returning data

--- apiCalls.js
+++ apiCalls.js
@@ -1,11 +1,11 @@
 const API_KEY = process.env.VUE_APP_API_KEY
-
 export const getPictures = async () => {
   const response = await fetch(`https://api.unsplash.com/search/photos/?client_id=${API_KEY}&query=dog`)
   if (!response.ok) {
-    throw Error(`There was an issue searching for synonyms for`)
+    throw Error(`There was an issue searching pictures`)
   }
-  const data = await response.json
+  const data = await response.json()
+  console.log('data-->', data)
   return data
 }
 
