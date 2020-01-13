64ea76a5226ce6f9b36df3085ce3143359a9bb0f
Zoe <zoekharvey@gmail.com>
2020-01-12 21:08:43 -0700

    Edit server.js

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-const environment = process.env.NODE_ENV || 'development';
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
+app.listen(port);
