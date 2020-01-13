968cf898d2d9c174e5f4622b0da2f6899312a3ba
Zoe <zoekharvey@gmail.com>
2020-01-12 21:47:46 -0700

    Add console log to trouble shoot variable

--- apiCalls.js
+++ apiCalls.js
@@ -1,5 +1,6 @@
 const API_KEY = process.env.VUE_APP_API_KEY
 export const getPictures = async (searchWord, page, perPage) => {
+  console.log(API_KEY)
   const response = await fetch(`https://api.unsplash.com/search/photos/?client_id=${API_KEY}&query=${searchWord}&page=${page}&per_page=${perPage}`)
   if (!response.ok) {
     throw Error(`There was an issue searching pictures`)
